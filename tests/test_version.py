4d736d53e69fa6779544fc2dae971b827c09eeaf
Waket Zheng <waketzheng@gmail.com>
2024-10-30 20:44:35 +0800

    ci: fix test error

--- tests/test_version.py
+++ tests/test_version.py
@@ -5,4 +5,4 @@ from asynctor import __version__
 
 def test_version():
     r = subprocess.run(["poetry", "version", "-s"], capture_output=True)
-    assert r.stdout.decode().strip() == __version__
+    assert r.stdout.decode().strip().split()[-1] == __version__
