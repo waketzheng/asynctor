69a522d84e55319e8369065008d77121a777e759
Waket Zheng <waketzheng@gmail.com>
2025-03-24 12:24:07 +0800

    Bump version: 0.6.9 → 0.7.0

--- asynctor/__init__.py
+++ asynctor/__init__.py
@@ -3,7 +3,7 @@ from .client import AsyncRedis
 from .timing import timeit
 from .utils import AsyncClientGenerator, AsyncTestClient, AttrDict, cache_attr
 
-__version__ = "0.6.9"
+__version__ = "0.7.0"
 __all__ = (
     "__version__",
     "AsyncClientGenerator",
