90d5020ef1d571d862c28f95f59b0cc84cd8d5bf
Waket Zheng <waketzheng@gmail.com>
2025-06-03 23:10:45 +0800

    ci: fixing py3.14 test version error

--- tests/test_version.py
+++ tests/test_version.py
@@ -6,5 +6,15 @@ from asynctor import __version__
 def test_version():
     r = subprocess.run(["pdm", "list", "--fields=name,version", "--csv"], capture_output=True)
     out = r.stdout.decode().strip()
-    me = [j for i in out.splitlines() if (j := i.split(","))[0] == "asynctor"][0]
-    assert me[-1] == __version__
+    try:
+        me = [j for i in out.splitlines() if (j := i.split(","))[0] == "asynctor"][0]
+    except IndexError:
+        print("------ out:")
+        print(out)
+        print("^" * 20)
+        from pathlib import Path
+
+        assert __version__ in Path("asynctor/__init__.py").read_text()
+
+    else:
+        assert me[-1] == __version__
