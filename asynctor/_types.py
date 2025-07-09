36ac582c4d1cbe1d00a4895f8957b6f7fa5708ea
Waket Zheng <waketzheng@gmail.com>
2025-07-09 17:18:50 +0800

    tests: omit `_types.py`

--- asynctor/_types.py
+++ asynctor/_types.py
@@ -1,6 +1,6 @@
 import sys
 
-if sys.version_info >= (3, 10):  # pragma: no cover
+if sys.version_info >= (3, 10):
     from typing import TypeAlias
 else:
     from typing_extensions import TypeAlias
