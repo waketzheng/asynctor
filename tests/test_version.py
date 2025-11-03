b8f2bcaa9a881fd995e338b7cbae4e06fc9270d2
Waket Zheng <waketzheng@gmail.com>
2025-11-03 20:56:36 +0800

    tests: use `uv pip list` instead of `pdm list` for version test

--- tests/test_version.py
+++ tests/test_version.py
@@ -1,16 +1,10 @@
 import subprocess
-from pathlib import Path
 
-from asynctor import __version__
+from asynctor import __package__, __version__
 
 
 def test_version():
-    r = subprocess.run(["pdm", "list", "--fields=name,version", "--csv"], capture_output=True)
+    r = subprocess.run(["uv", "pip", "list"], capture_output=True)
     out = r.stdout.decode().strip()
-    try:
-        me = [j for i in out.splitlines() if (j := i.split(","))[0] == "asynctor"][0]
-    except IndexError:
-        # TODO: remove this when deps of python3.14 can be install by pdm in ci
-        assert __version__ in Path("asynctor/__init__.py").read_text()
-    else:
-        assert me[-1] == __version__
+    me = [j for i in out.splitlines() if (j := i.split())[0] == __package__][0]
+    assert me[1] == __version__
