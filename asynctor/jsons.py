a38c49a44eeb5258a719e1ceeb2f3d9ddccaf990
Waket Zheng <waketzheng@gmail.com>
2025-11-04 12:33:27 +0800

    feat: add asynctor.jsons.FastJson

--- asynctor/jsons.py
+++ asynctor/jsons.py
@@ -2,7 +2,15 @@ from __future__ import annotations
 
 import functools
 from collections.abc import Callable
-from typing import Any
+from pathlib import Path
+from typing import Any, Literal, TypeAlias, overload
+
+from .exceptions import UnsupportedError
+
+BasicJsonType: TypeAlias = str | bool | int | float | None
+JsonAbleType: TypeAlias = dict[str, Any] | list[Any] | BasicJsonType
+StrictJsonType: TypeAlias = dict[str, JsonAbleType] | list[JsonAbleType] | BasicJsonType
+DumpedJsonType: TypeAlias = str | bytes | bytearray
 
 try:
     import orjson
@@ -20,9 +28,7 @@ except ImportError:
     ) -> bytes:
         return json_dumps(obj, pretty=pretty, default=default).encode()
 
-    def json_loads(
-        obj: str | bytes | bytearray,
-    ) -> dict[str, Any] | list[Any] | str | bool | int | float | None:
+    def json_loads(obj: DumpedJsonType) -> JsonAbleType:
         return json.loads(obj)
 else:
 
@@ -37,9 +43,7 @@ else:
     ) -> str:
         return json_dump_bytes(obj, pretty=pretty, default=default).decode()
 
-    def json_loads(
-        obj: str | bytes | bytearray,
-    ) -> dict[str, Any] | list[Any] | str | bool | int | float | None:
+    def json_loads(obj: DumpedJsonType) -> JsonAbleType:
         return orjson.loads(obj)
 
 
@@ -56,3 +60,44 @@ json_dumps.__doc__ = """Serialize ``obj`` to a JSON formatted string
 :param pretty: whether indent to humanize
 """
 json_loads.__doc__ = """Deserialize JSON to Python objects."""
+
+
+class FastJson:
+    @overload
+    @classmethod
+    def dumps(cls, obj: Any, output: Literal["str"], *, pretty: bool = False) -> str: ...
+    @overload
+    @classmethod
+    def dumps(
+        cls, obj: Any, output: Literal["bytes"] | Path = "bytes", *, pretty: bool = False
+    ) -> bytes: ...
+
+    @classmethod
+    def dumps(
+        cls,
+        obj: Any,
+        output: Literal["str", "bytes"] | Path = "bytes",
+        *,
+        pretty: bool = False,
+    ) -> str | bytes:
+        """Serialize ``obj`` to a JSON formatted string or bytes
+
+        :param obj: object to be dumped.
+        :param output: if 'str' return utf-8 string, elif 'bytes' return utf-8 encoded bytes,
+                    elif Path write dumped content to it and return bytes, else raise ValueError.
+        :param pretty: whether indent to humanize.
+        """
+        match output:
+            case "bytes" | Path():
+                content = json_dump_bytes(obj, pretty=pretty)
+                if isinstance(output, Path):
+                    output.write_bytes(content)
+                return content
+            case "str" | "string":
+                return json_dumps(obj, pretty=pretty)
+            case x if x is str:  # support: FastJson.dumps(obj, output=str)
+                return json_dumps(obj, pretty=pretty)
+            case _:
+                raise UnsupportedError(f"Unsupported output format: {output!r}")
+
+    loads = staticmethod(json_loads)
--- tests/test_jsons.py
+++ tests/test_jsons.py
@@ -1,15 +1,71 @@
-from asynctor.jsons import json_dump_bytes, json_dumps, json_loads
+from datetime import datetime
+from pathlib import Path
+
+import pytest
+
+from asynctor.exceptions import AsynctorError, UnsupportedError
+from asynctor.jsons import FastJson, json_dump_bytes, json_dumps, json_loads
+
+
+def default(obj):
+    if isinstance(obj, datetime):
+        return str(obj)
 
 
 def test_dumps():
     assert json_dumps({1: 1}) == '{"1":1}'
     assert json_dumps({1: 1}, pretty=True) == '{\n  "1": 1\n}'
+    dt = datetime(2025, 11, 13)
+    with pytest.raises(TypeError):
+        json_dumps({1: dt})
+    assert json_dumps({1: dt}, default=default) == '{"1":"2025-11-13 00:00:00"}'
 
 
 def test_dump_bytes():
     assert json_dump_bytes({1: 1}) == b'{"1":1}'
+    assert json_dump_bytes({1: 1}, pretty=True) == b'{\n  "1": 1\n}'
 
 
 def test_loads():
     assert json_loads(b'{"1":1}') == {"1": 1}
     assert json_loads('{"1":1}') == {"1": 1}
+
+
+def test_fast_json():
+    assert FastJson.loads(b'{"1":1}') == {"1": 1}
+    assert FastJson.loads.__doc__ == json_loads.__doc__
+    assert FastJson.dumps({1: 1}, "str") == '{"1":1}'
+    assert FastJson.dumps({1: 1}, "string") == '{"1":1}'  # type:ignore
+    assert FastJson.dumps({1: 1}, output="str") == '{"1":1}'
+    assert FastJson.dumps({1: 1}, output=str) == '{"1":1}'  # type:ignore
+    assert FastJson.dumps({1: 1}, "str", pretty=True) == '{\n  "1": 1\n}'
+    assert FastJson.dumps({1: 1}, pretty=True, output="str") == '{\n  "1": 1\n}'
+    assert FastJson.dumps({1: 1}) == b'{"1":1}'
+    assert FastJson.dumps({1: 1}, "bytes") == b'{"1":1}'
+    assert FastJson.dumps({1: 1}, output="bytes") == b'{"1":1}'
+    assert FastJson.dumps({1: 1}, pretty=True) == b'{\n  "1": 1\n}'
+    assert FastJson.dumps({1: 1}, "str") == '{"1":1}'
+    dt = datetime(2025, 11, 13)
+    with pytest.raises(TypeError):
+        FastJson.dumps({1: dt})
+    # For `default` case, use `json_dumps` or `json_dump_bytes` instead
+    with pytest.raises(TypeError):
+        FastJson.dumps({1: dt}, default=default)  # type:ignore
+
+
+def test_fast_json_unsupported_output():
+    with pytest.raises(ValueError):
+        FastJson.dumps({1: 1}, output=bytes)  # type:ignore
+    with pytest.raises(AsynctorError):
+        FastJson.dumps({1: 1}, output=bytes)  # type:ignore
+    with pytest.raises(UnsupportedError):
+        FastJson.dumps({1: 1}, output=bytes)  # type:ignore
+
+
+def test_fast_json_write(tmp_work_dir):
+    p = Path("a.json")
+    assert FastJson.dumps({1: 1}, p) == b'{"1":1}'
+    assert p.read_bytes() == b'{"1":1}'
+    assert FastJson.dumps({1: 1}, p, pretty=True) == b'{\n  "1": 1\n}'
+    assert p.read_bytes() == b'{\n  "1": 1\n}'
+    assert FastJson.dumps({1: 1}, pretty=True, output=p) == b'{\n  "1": 1\n}'
