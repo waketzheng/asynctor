d598539449aba1598889e1543651dd115e21105e
Waket Zheng <waketzheng@gmail.com>
2025-12-11 01:00:46 +0800

    Bump version: 0.11.4 → 0.11.5

--- asynctor/__init__.py
+++ asynctor/__init__.py
@@ -3,7 +3,7 @@ from .client import AsyncRedis
 from .timing import Timer, timeit
 from .utils import AsyncClientGenerator, AsyncTestClient, AttrDict, Shell, cache_attr
 
-__version__ = "0.11.4"
+__version__ = "0.11.5"
 __all__ = (
     "__version__",
     "AsyncClientGenerator",
